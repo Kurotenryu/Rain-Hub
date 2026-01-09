7f063bd77b5266148917928670aa6eb377b1cd0e
Kurotenryu <lamzcode.xyz@gmail.com>
2026-01-09 07:36:29 +0700

    RainRed.lua

--- RainRed.lua
+++ RainRed.lua
@@ -104,7 +104,7 @@ end
 task.spawn(function()
   while task.wait(1.5) do
     pcall(function()
-      if _G.AutoTS then
+      if _G.AutoTS or _G.AutoElite or _G.AutoEliteHop then
         _G.IsTweening = true
         local BodyVelocityClip = game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild("BodyAntiVelocityClip") or Instance.new("BodyVelocity", game.Players.LocalPlayer.Character.HumanoidRootPart)
         BodyVelocityClip.Name = "BodyAntiVelocityClip"
